8a07c4803a9830d91ede86e0e485754d564595e5
Anthony <awatson31911@gmail.com>
2019-05-09 12:33:16 -0400

    fixes start up bugs

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-"use strict";
+
 
 const express = require('express');
 const app = express(); // Creates an instance of express application
 const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 /* ------------------------------ Middleware ------------------------------------------- */
 
@@ -17,35 +17,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /* --------------- Static ------------------- */
-// app.use(express.static(path.join(__dirname, '..', 'node_modules'))); // serves node_modules
-// app.use(express.static(path.join(__dirname, '..', 'dist'))); //serves webpack bundle.js
-// app.use(express.static(path.join(__dirname, '..', 'client'))); //serves css/js files
+app.use(express.static(path.join(__dirname, '..', 'node_modules'))); // serves node_modules
+app.use(express.static(path.join(__dirname, '..', 'dist'))); //serves webpack bundle.js
+app.use(express.static(path.join(__dirname, '..', 'client'))); //serves css/js files
 
 /* --------------- Routing ------------------- */
 //app.use('/api', require('./routes'));
 
 
 /* ------------------------------ Default index.html ------------------------------------------- */
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/views/index.html'));
 
 });
 
 /* ----------------------------------- Error Handling ------------------------------------------- */
-app.use(function(err, req ,res, next) {
+app.use((err, req ,res) => {
   console.error(err);
   console.error(err.stack);
   res.status(err.status || 500).send(err.message || 'Internal server error.');
-})
+});
 
 /* ------------------------------ ~ Start Server ~ ------------------------------------------- */
-app.listen(port, function() {
-  console.log("Knock, Knock");
-  setTimeout(function() {
-    console.log("Who's there?")
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log('Knock, Knock');
+  setTimeout(() => {
+    // eslint-disable-next-line no-console
+    console.log('Who\'s there?');
   }, 1000);
-  setTimeout(function() {
+  setTimeout(() => {
+    // eslint-disable-next-line no-console
     console.log(`...We're
-    listening (port: 3000).... `)
+    listening (port:3000).... `);
   }, 3000);
-})
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: './client/js/main.js',
-    output: {
-        path: path.join(__dirname,'dist'),
-        publicPath: '/client',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader'
-            }
-          },
-          {
-              test: /\.css?|\.scss$/,
-              loader: ['style-loader', 'css-loader']
-          }
-        ]
+  devtool: 'source-map',
+  entry: './client/js/main.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    //publicPath: '/client/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+
+      },
+      {
+        test: /\.css?|\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader'
       }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
